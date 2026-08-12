75238cac6a4e539ea81482f2acae5ee7f779f5c8
catwalk2026 <matsumoto.koki@abcash.co.jp>
2026-08-13 03:29:04 +0900

    main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ import atexit
 
 # ▼▼▼ ここに取得した2つの鍵を貼り付けてください ▼▼▼
 LINE_CHANNEL_ACCESS_TOKEN = """rlJ1YRFK3hCEYnrfCe5k9kO2gjyX3YkqhfdAvnT28lWoC/9Q6NTtPdBNvGU6jVWunuf7k6NPAg/d2r39X+IxD4mlNjs2bH4krV2B7zWilto5IHSvo7QXkKbIxa0GNvVN2SK9b2AH03Rs/M6VrJBIlwdB04t89/1O/w1cDnyilFU=""
-LINE_CHANNEL_SECRET = """c8caf38acc62174908dcff1f782621f6"""
+LINE_CHANNEL_SECRET = "c8caf38acc62174908dcff1f782621f6"
 # ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
